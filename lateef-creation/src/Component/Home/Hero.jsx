274e7cf2fd97672677fed777f6a9d6f789e0a8b5
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-09-18 22:52:19 +0600

    Update Hero & Navbar Responsive

--- lateef-creation/src/Component/Home/Hero.jsx
+++ lateef-creation/src/Component/Home/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
                         <p className='text-white text-base lg:text-xl'>WELCOME TO LATEEF CREATION</p>
                         <h1 className='text-4xl lg:text-7xl my-3 text-[#969696]'>I AM YOUR<br></br> <span className='text-white'>UNIQUE</span> DESIGN<br></br> SOLUTION</h1>
                         <p className='hero__title text-sm lg:text-xl uppercase text-[#969696]'>Have a project in mind? would you like to share it? feel free to reach out by writing an e-mail  </p>
-                        <button className='py-3 px-6 bg-white mt-9'>let's work together</button>
+                        <button className='py-3 px-6 bg-white mt-6 lg:mt-9 text-sm lg:text-base'>let's work together</button>
                     </div>
                 </div>
                 <div className="hero__main__right mt-10 lg:mt0 w-full lg:w-2/5">
--- lateef-creation/src/Component/NavMenu/NavMenu.jsx
+++ lateef-creation/src/Component/NavMenu/NavMenu.jsx
@@ -6,14 +6,14 @@ import './NavMenu.css'
 
 const NavMenu = () => {
     return (
-        <div className='navmenu__main pt-8'>
+        <div className='navmenu__main pt-8 lg:mx-0 mx-3'>
             <div className="container mx-auto max-w-screen-xl flex justify-between items-center">
                 <div className="logo">
-                    <Link to='/'><img className='' src={laatefCreationLogo} alt="" /></Link>
+                    <Link to='/'><img className='w-32 lg:w-min' src={laatefCreationLogo} alt="" /></Link>
                 </div>
                 <div className="menu flex items-center">
-                    <button className='hire__button text-base bg-white py-3 px-6 mr-7'><a href="mailto:lateefshuvro@gmail.com">Hire me</a></button>
-                    <button className='menu__text text-2xl text-white'>
+                    <button className='hire__button text-base bg-white py-3 px-6 mr-7 hidden lg:block'><a href="mailto:lateefshuvro@gmail.com">Hire me</a></button>
+                    <button className='menu__text text-lg lg:text-2xl text-white'>
                         <span><img className='ml-auto' src={menu} alt="" /></span>
                         <p className='text-end tracking-[.6em] mr-[-10px] font-semibold'>Menu</p>
                     </button>
