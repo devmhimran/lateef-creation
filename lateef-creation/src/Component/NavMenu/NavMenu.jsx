18533f81e3f984caaad89c46ba9d23f7e6ade9f8
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-09-21 18:42:00 +0600

    Added Responsive Dropdown Navbar

--- lateef-creation/src/Component/NavMenu/NavMenu.jsx
+++ lateef-creation/src/Component/NavMenu/NavMenu.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import laatefCreationLogo from '../../assets/lateef-creation-logo.png'
 import menu from '../../assets/menu.svg'
 import './NavMenu.css'
+import { HiX } from 'react-icons/hi';
 
 const NavMenu = () => {
+    const [open, setOpen] = useState(false);
+    console.log(open)
     return (
         <div className='navmenu__main relative pt-8 lg:mx-0 mx-3'>
             <div className="container mx-auto max-w-screen-xl flex justify-between items-center">
@@ -13,14 +16,27 @@ const NavMenu = () => {
                 </div>
                 <div className="menu flex items-center">
                     <button className='hire__button text-base bg-white py-3 px-6 mr-7 hidden lg:block'><a href="mailto:lateefshuvro@gmail.com">Hire me</a></button>
-                    <button className='menu__text text-lg lg:text-2xl text-white'>
+                    <button onClick={()=> setOpen(!open)} className='menu__text text-lg lg:text-2xl text-white'>
                         <span><img className='ml-auto' src={menu} alt="" /></span>
                         <p className='text-end tracking-[.6em] mr-[-10px] font-semibold'>Menu</p>
                     </button>
                 </div>
             </div>
-            <div className="dropdown__menu">
+            <div className={`dropdown__menu w-full h-screen  bg-[#1F1F1F] duration-500 ease-in z-50 fixed ${open ? 'top-0' : 'top-[-1000px]'}`}>
+                    <div onClick={() => setOpen(!open)} className="dropdown__close absolute right-[5%] top-[5%] cursor-pointer">
+                        <HiX className='text-3xl lg:text-5xl text-white'></HiX>
+                    </div>
+                    <div className="dropdown__item flex justify-center items-center w-full h-screen">
 
+                        <ul className='text-center'>
+                            <li className='my-3 lg:my-5 text-lg lg:text-3xl text-white'><a href="/">Home</a></li> <hr className='border-gray-800 ' />
+                            <li className='my-3 lg:my-5 text-lg lg:text-3xl text-white'><a href="/category/website-uiux">Website UiUx Design</a></li> <hr className='border-gray-800 ' />
+                            <li className='my-3 lg:my-5 text-lg lg:text-3xl text-white'><a href="/category/app-uiux">APP UiUx Design</a></li> <hr className='border-gray-800 ' />
+                            <li className='my-3 lg:my-5 text-lg lg:text-3xl text-white'><a href="/category/creative-logo-design">Creative Logo Design</a></li> <hr className='border-gray-800 ' />
+                            <li className='my-3 lg:my-5 text-lg lg:text-3xl text-white'><a href="/category/latest-instagram-post">LATEST INSTAGRAM POST</a></li> <hr className='border-gray-800 ' />
+                            <li className='my-3 lg:my-5 text-lg lg:text-3xl text-white'><a href="/category/learning-video-tutorial">LEARNING VIDEO TUTORIAL</a></li> <hr className='border-gray-800 ' />
+                        </ul>
+                    </div>     
             </div>
         </div>
     );
