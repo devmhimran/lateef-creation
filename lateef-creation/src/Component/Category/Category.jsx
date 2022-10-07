950165dfba409d0803006b4a004879f118c4d38d
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-10-08 05:22:54 +0600

    Update Hosting

--- lateef-creation/src/Component/Category/Category.jsx
+++ lateef-creation/src/Component/Category/Category.jsx
@@ -13,7 +13,7 @@ const Category = () => {
             .then(data => setPortfolio(data))
     }, []);
     return (
-        <div className='category__main container mx-auto max-w-screen-xl py-20 lg:py-32'>
+        <div className="category__main container mx-auto max-w-screen-xl py-20 lg:py-32">
             <PageTitle title={categoryNameMain}></PageTitle>
             <div className="category__content mx-3 lg:mx-0">
                 <h1 className='text-white text-3xl lg:text-5xl text-center uppercase'>{categoryNameMain}</h1>
